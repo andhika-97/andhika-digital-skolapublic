f850414b7c065cb6491df3df82680a397469401b
andhika-97 <afajar104@gmail.com>
2026-04-19 15:26:08 +0700

    tugas mocha mas  ga bisa jalan ini

---
+++ tugas mocha.js
@@ -0,0 +1,23 @@
+import fetch from "node-fetch";
+import { expect } from "chai";
+import Ajv from "ajv";
+
+describe("cobain test", function () {
+    const baseURL = "https://reqres.in";
+
+    it("READ - Get single user", async function () {
+        // const response = await fetch(`${baseURL}/api/users?page=2`);
+        const response = await fetch("https://reqres.in/api/users/2", {
+            headers: { "x-api-key": "reqres_e21900c2783e456099c7a77163a91c7e" },
+            method: "GET",
+        });
+
+        expect(response.status, 'testcase nya error').to.equal(200);
+        
+        const hasil=await response.json();
+        console.log(hasil.data.id);
+        expect(hasil.data.id, 'idnya salah').to.equal(4);
+
+    });
+});
+

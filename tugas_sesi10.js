fbfc0a24cb62e1e33051a0181e1834a2dcc65d35
andhika-97 <afajar104@gmail.com>
2026-05-02 13:35:44 +0700

    revisi HOOK

--- tugas_sesi10.js
+++ tugas_sesi10.js
@@ -2,9 +2,11 @@ const { Builder, By, until } = require('selenium-webdriver');
 const assert = require('assert');
 const chrome = require('selenium-webdriver/chrome');
 
-describe('tugas sesi 9', ()=>{
-     it('Visit SauceDemo dan cek page title', async function () {
-       let options = new chrome.Options();
+describe('tugas sesi 10', ()=>{
+    let driver;
+
+    beforeEach =(async() =>{
+               let options = new chrome.Options();
        options.addArguments('--incognito');
         const driver = await new Builder()
             .forBrowser('chrome')
@@ -12,62 +14,45 @@ describe('tugas sesi 9', ()=>{
             .build();
 
         await driver.get('https://www.saucedemo.com');
+    })
+
+
+    afterEach =(async() =>{
+        await driver.sleep(5000);
+        await driver.quit();
+    })
+     it('correct ID', async function () {
+
+
+
         await driver.findElement(By.id ('user-name')).sendKeys('standard_user');
         await driver.findElement(By.id('password')).sendKeys('secret_sauce');
         await driver.findElement(By.id('login-button')).click();
-        await driver.sleep(5000);
-        await driver.quit();
+
         
         
      });
       it('wrong ID', async function () {
-       let options = new chrome.Options();
-       options.addArguments('--incognito');
-        const driver = await new Builder()
-            .forBrowser('chrome')
-            .setChromeOptions(options)
-            .build();
 
-        await driver.get('https://www.saucedemo.com');
         await driver.findElement(By.id ('user-name')).sendKeys('wrong_id');
         await driver.findElement(By.id('password')).sendKeys('secret_sauce');
         await driver.findElement(By.id('login-button')).click();
-        await driver.sleep(5000);
-        await driver.quit();
         
         
      });
      it('wrong password', async function () {
-       let options = new chrome.Options();
-       options.addArguments('--incognito');
-        const driver = await new Builder()
-            .forBrowser('chrome')
-            .setChromeOptions(options)
-            .build();
 
-        await driver.get('https://www.saucedemo.com');
         await driver.findElement(By.id ('user-name')).sendKeys('standard_user');
         await driver.findElement(By.id('password')).sendKeys('wrong_password');
         await driver.findElement(By.id('login-button')).click();
-        await driver.sleep(5000);
-        await driver.quit();
         
         
      });
-          it('user locket', async function () {
-       let options = new chrome.Options();
-       options.addArguments('--incognito');
-        const driver = await new Builder()
-            .forBrowser('chrome')
-            .setChromeOptions(options)
-            .build();
+    it('user locket', async function () {
 
-        await driver.get('https://www.saucedemo.com');
         await driver.findElement(By.id ('user-name')).sendKeys('locket_out_user');
         await driver.findElement(By.id('password')).sendKeys('secret_sauce');
         await driver.findElement(By.id('login-button')).click();
-        await driver.sleep(5000);
-        await driver.quit();
         
         
      });
